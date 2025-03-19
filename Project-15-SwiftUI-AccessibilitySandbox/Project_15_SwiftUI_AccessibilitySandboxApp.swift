ab0cab45949d379dcd43fdee8f896d4c347bc939
kevin cuadros <kevindavidcuadros@gmail.com>
2025-03-19 14:26:45 -0500

    Initial Commit

---
+++ Project-15-SwiftUI-AccessibilitySandbox/Project_15_SwiftUI_AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_15_SwiftUI_AccessibilitySandboxApp.swift
+//  Project-15-SwiftUI-AccessibilitySandbox
+//
+//  Created by Kevin Cuadros on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_15_SwiftUI_AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
